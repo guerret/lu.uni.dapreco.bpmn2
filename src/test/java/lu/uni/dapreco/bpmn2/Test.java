98bc3ba02b275dddb31a4e09d17ed72914e8eac6
Cesare Bartolini <cesare.bartolini@uni.lu>
2018-06-22 15:45:54 +0200

    Path fixes.

--- src/test/java/lu/uni/dapreco/bpmn2/Test.java
+++ src/test/java/lu/uni/dapreco/bpmn2/Test.java
@@ -1,19 +1,17 @@
 package lu.uni.dapreco.bpmn2;
 
-import java.io.File;
-
 public class Test {
 
 	private final static String resDir = "resources";
 
 	private final static String lrmlName = "rioKB_GDPR.xml";
 	private final static String lrmlURI = "https://raw.githubusercontent.com/dapreco/daprecokb/master/gdpr/" + lrmlName;
-	// private final static String lrmlLocal = resDir + File.separator + lrmlName;
+	// private final static String lrmlLocal = resDir +l "/" + lrmlName;
 
 	private final static String aknName = "akn-act-gdpr-full.xml";
 	private static final String aknURI = "https://raw.githubusercontent.com/guerret/lu.uni.dapreco.bpmn2/master/"
-			+ resDir + File.separator + aknName;
-	// private static final String aknLocal = resDir + File.separator + aknName;
+			+ resDir + "/" + aknName;
+	// private static final String aknLocal = resDir + "/" + aknName;
 
 	private static String aknPrefix = "GDPR";
 	private static String ontoPrefix = "prOnto";
